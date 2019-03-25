e0d07eaefd0efbf318be8133e56e9b0ed405f644
aambrioso1 <44902917+aambrioso1@users.noreply.github.com>
2019-03-24 20:56:22 -0400

    Add files via upload

--- Dictionary for Cards.py
+++ Dictionary for Cards.py
@@ -7,13 +7,13 @@ Colors = ['Black', 'Red']
 Vals = [[1,11]]+[i for i in range(2,11)]+[10,10,10]
 
 for i in range(52):
-	print(Dens[i%13], Suits[i%4], Vals[i%13], Colors[i%2])
+	print(Dens[i % 13], Suits[i % 4], Vals[i % 13], Colors[i % 2])
 
-Cards = {(Dens[i%13], Suits[i%4]): {'Value': Vals[i%13], 'Suit': Suits[i%4],'Color': Colors[i%2]} for i in range(52)}
+Cards = {(Dens[i % 13], Suits[i % 4]): {'Value': Vals[i % 13], 'Suit': Suits[i % 4],'Color': Colors[i % 2]} for i in range(52)}
 
 #Print the value of the ace of spades.
 print('\n')
 print(Cards[('Ace', 'Spades')]['Value'], '\n')
 
-#Print the value color of the two of hearts.
+#Print the color of the two of hearts.
 print(Cards[('Two', 'Hearts')]['Color'])
--- fib.py
+++ fib.py
@@ -1,8 +1,11 @@
 def fib(n):
+
 	if n==0:
 		return 0
 	if n==1:
 		return 1
 	total=fib(n-1)+fib(n-2)
 	return total
-print(fib(20))
+
+
+print(fib(30))
