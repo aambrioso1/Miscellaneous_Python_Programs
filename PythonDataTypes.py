305acd24888f4641237c67c166a2468c0041c509
aambrioso1 <44902917+aambrioso1@users.noreply.github.com>
2019-04-12 07:28:17 -0400

    Add files via upload

---
+++ PythonDataTypes.py
@@ -0,0 +1,92 @@
+# Number types:  integers and floating decimals
+x = 1
+y = 3.14
+z = True
+print(x,'is a',type(x),'.')
+print('%f is a %s' % (y,type(y)))
+print('%f is a %s' % (y,type(y)))
+print('%s is a %s' % (z,type(z)))
+print(type(True))
+
+#strings
+#str='Erika'
+
+#print(str, str[2:4], str[3:], str[:3])
+
+
+#lists
+#list1=['a', 1, 2, 'Alex', '3']
+#print('%s is a %s' % (list1,type(list1)))
+#str2=list1[4]
+#print('%s is a %s' % (str2, type(str2)))
+#i=int(str2)
+#print('%i is a %s' % (i, type(i)))
+
+
+
+#sets
+#set1 = {1,1,1,2,2,3}
+#print(len(set1))
+
+#set2 = {3,2,1}
+#print(set1 ==  set2)
+#print('set1 is a ', set1)
+
+
+
+#tuples
+#tup1 = (1,2,3)
+
+#comprehension
+#list2 = [n**3 for n in range(10)]
+
+#print(list2)
+
+#conditionals
+#T=True
+#F=False
+#print(T and F, T or F, not F)
+
+#x = 10
+#y = 9
+
+#if first > second:
+    #print('The first number is bigger.' )
+
+#if first < second:
+    #print('The first number is bigger than the second.')
+#else: print('The second number is bigger or equal to the first')
+
+
+#Loops
+#for i in range(0,10):
+    #print(i)
+#for i in {'a', 'b', 'c', 'd'}:
+    #print(i)
+
+#go = True
+#while(go == True):
+    #str=input('Keep going? Y or N?\n')
+    #if str == 'N':
+        #go = False
+
+
+
+#comprehension                     
+#list2 = [n**3 for n in range(10)]
+
+
+
+#functions
+#def printline(n):
+    #str = ''
+    #for i in range(n):
+        #str += '*'
+    #return print(str)
+
+#printline(3)
+#printline(10)
+#for i in range(100):
+    #printline(i)
+
+
